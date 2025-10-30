77765588f03605f99bda550a60cc56a8ed69db71
SeguOostdijk <seguoostdijk730@gmailcom>
2025-10-30 10:30:22 -0300

    agrega ventana nuevo prestamo

---
+++ src/main/java/com/application/gestiondenotebooks/controller/NuevoPrestamoController.java
@@ -0,0 +1,8 @@
+package com.application.gestiondenotebooks.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NuevoPrestamoController {
+
+}
--- src/main/java/com/application/gestiondenotebooks/controller/VentanaPrincipalController.java
+++ src/main/java/com/application/gestiondenotebooks/controller/VentanaPrincipalController.java
@@ -1,10 +1,12 @@
 package com.application.gestiondenotebooks.controller;
 
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -12,4 +14,19 @@ import java.io.IOException;
 @Component
 public class VentanaPrincipalController {
 
+    @Autowired
+    private ApplicationContext context; // Spring
+
+    public void irANuevoPrestamo(javafx.event.ActionEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com.application.gestiondenotebooks/NuevoPrestamo.fxml"));
+        loader.setControllerFactory(context::getBean);  // <-- para que Spring cree el controller
+        Parent root = loader.load();
+        // obtener el Stage actual desde el botón que disparó el evento
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setTitle("Agregar Nuevo Préstamo");
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
